9a0b29ebd84acdcdc5fddf8e8633eba1aabde11d
ranaritika <ranaritika22@gmail.com>
2018-03-16 11:53:49 +0530

    second commit

---
+++ src/com/pack/generics/CacheString.java
@@ -0,0 +1,17 @@
+
+package com.pack.generics;
+
+
+public class CacheString {
+  
+    private String message;
+  
+    public void add(String message){
+        this.message = message;
+    }
+  
+    public String get(){
+        return this.message;
+    }  
+  
+}
